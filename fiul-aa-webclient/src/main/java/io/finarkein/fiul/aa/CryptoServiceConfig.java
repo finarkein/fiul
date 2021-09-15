1fe4612fb7615b758a79c9ed549659780655e6b6
Prathamesh Mungarwadi <prathmesh.mungarwadi@finarkein.com>
2021-09-15 23:41:48 +0530

    Added null case handling in NotificationValidator

--- fiul-aa-webclient/src/main/java/io/finarkein/fiul/aa/CryptoServiceConfig.java
+++ fiul-aa-webclient/src/main/java/io/finarkein/fiul/aa/CryptoServiceConfig.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (C) 2021 Finarkein Analytics Pvt. Ltd.
+ * All rights reserved This software is the confidential and proprietary information of Finarkein Analytics Pvt. Ltd.
+ * You shall not disclose such confidential information and shall use it only in accordance with the terms of the license
+ * agreement you entered into with Finarkein Analytics Pvt. Ltd.
+ */
 package io.finarkein.fiul.aa;
 
 import org.springframework.beans.factory.annotation.Value;
