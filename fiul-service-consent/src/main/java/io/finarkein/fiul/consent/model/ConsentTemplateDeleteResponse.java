07b4279977ed7d47005f97d6436eb88c519a33c8
primary <primary@finarkein.com>
2021-09-19 23:35:50 +0530

    build: aa-commons upgraded to 0.3.0
    - test fix

--- fiul-service-consent/src/main/java/io/finarkein/fiul/consent/model/ConsentTemplateDeleteResponse.java
+++ fiul-service-consent/src/main/java/io/finarkein/fiul/consent/model/ConsentTemplateDeleteResponse.java
@@ -9,10 +9,12 @@ package io.finarkein.fiul.consent.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor
+@NoArgsConstructor
 public class ConsentTemplateDeleteResponse {
     private String consentTemplateId;
     private boolean deleted;
