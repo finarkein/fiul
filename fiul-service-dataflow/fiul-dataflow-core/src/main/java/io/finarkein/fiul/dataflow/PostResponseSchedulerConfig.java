c7a4692d351db5daa143427d97fb958304d6b172
primary <primary@finarkein.com>
2022-01-28 16:24:18 +0530

    wip: added publishOn with configurable scheduler
    - fiul.post-response.scheduler-type:immediate
      - added to offload the processing of the response received from AA
      - currently it is done only for easyDataFlow FI/Request and FI/Fetch flow
      - it is dedicated boundedElastic scheduler
      - default is immediate
    FIU-104

---
+++ fiul-service-dataflow/fiul-dataflow-core/src/main/java/io/finarkein/fiul/dataflow/PostResponseSchedulerConfig.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2021 Finarkein Analytics Pvt. Ltd.
+ * All rights reserved This software is the confidential and proprietary information of Finarkein Analytics Pvt. Ltd.
+ * You shall not disclose such confidential information and shall use it only in accordance with the terms of the license
+ * agreement you entered into with Finarkein Analytics Pvt. Ltd.
+ */
+package io.finarkein.fiul.dataflow;
+
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.Optional;
+
+@Configuration
+@Getter
+@Log4j2
+public class PostResponseSchedulerConfig {
+
+    @ToString.Include
+    private final String schedulerType;
+
+    @ToString.Exclude
+    private final Scheduler scheduler;
+
+    static final String BOUNDED_ELASTIC = "boundedElastic";
+
+    public PostResponseSchedulerConfig(@Value("${fiul.post-response.scheduler-type:immediate}") String schedulerType,
+                                       @Value("${fiul.post-response.scheduler-name:response-processor}") String responseProcessor) {
+        this.schedulerType = schedulerType;
+        if (BOUNDED_ELASTIC.equalsIgnoreCase(schedulerType)) {
+            final int threadCap = Optional
+                    .ofNullable(System.getProperty("fiul.post-response.scheduler.size"))
+                    .map(Integer::parseInt)
+                    .orElseGet(() -> 10 * Runtime.getRuntime().availableProcessors());
+            final int queueSize = Optional
+                    .ofNullable(System.getProperty("fiul.post-response.scheduler.queue-size"))
+                    .map(Integer::parseInt)
+                    .orElse(100000);
+            scheduler = Schedulers.newBoundedElastic(threadCap, queueSize, responseProcessor);
+            log.info("PostResponseSchedulerConfig = schedulerType:{}, threadCap:{}, queueSize:{}", schedulerType
+                    , threadCap, queueSize);
+        } else {
+            scheduler = Schedulers.immediate();
+        }
+    }
+}
